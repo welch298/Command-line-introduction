b9a420377e7d9353d60fa0c73bb9b19f8e25d27d
hamer073 <hamer073@morris.umn.edu>
2018-09-02 10:06:05 -0500

    Fix the argument ordering for the tar command so now it propely decompresses and extracts the files to the target location
    Changed how the directory is changed so it goes into the folder of extracted files not just the temp folder

--- compiling/extract_and_compile.sh
+++ compiling/extract_and_compile.sh
@@ -2,7 +2,7 @@
 #Argument two which should be a file location is $2
 
 #Decompressing and extracting the files
-tar zx -f ./NthPrime.tgz $2
-cd $2
+tar -xzf NthPrime.tgz -C $2
+cd $2/NthPrime
 gcc -o NthPrime main.c nth_prime.c nth_prime.h
 ./NthPrime $1
