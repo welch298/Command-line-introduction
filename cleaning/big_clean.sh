70ab8fc68760251da8444679f2f58f83c6627ded
hamer073 <hamer073@morris.umn.edu>
2018-09-04 10:27:40 -0500

    Add a reference to the starting directory for use throughout the file
    Add rm -rf FilesToDelete for cleanup purposes
    
    Add a tar call to create the new cleaned archive
    
    Add a mv call to move the new archive back to the starting directory

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -1,7 +1,7 @@
 tar -xzf $1 -C $2
 NewDirectory=$(basename $1 ".tgz")
-echo $NewDirectory
-cd $2/big_dir
+StartingDirectory=$(pwd)
+cd $2/$NewDirectory
 grep -l 'DELETE ME!' * > FilesToDelete
 FileToBeRemoved=$(head -n 1 FilesToDelete)
 while [ "$FileToBeRemoved" != '' ]
@@ -10,3 +10,8 @@ do
 	tail -n +2 'FilesToDelete' > 'FilesToDelete.tmp' && mv 'FilesToDelete.tmp' 'FilesToDelete'
 	FileToBeRemoved=$(head -n 1 FilesToDelete)
 done
+rm -rf FilesToDelete
+cd ../
+tar -czvf cleaned_$NewDirectory.tgz $NewDirectory
+mv cleaned_$NewDirectory.tgz $StartingDirectory
+cd $StartingDirectory
